109014dc54e339ab1767f4e1e922083644727734
Slakec <slakec@gmail.com>
2014-09-20 00:51:54 +0200

    Fixed test

--- test/testingtest.js
+++ test/testingtest.js
@@ -1,14 +1,18 @@
 (function(){
-	// če je kar koli narobe alert uporabnika
-	assertThat(true, "prva stopnicka");
+	window.onload = function() {
+		// če je kar koli narobe alert uporabnika
+		assertThat(true, "prva stopnicka");
 
-	var element = document.getElementById("test-results");
-	if (element === null) {
-		alert("Nisem ustvari div wraperja za testiranje");
-		return;
-	}
-	if (element.children.length !== 5) {
-		alert("Nisem ustvaril vseh potrebnih elementov");
-	}
+		var element = document.getElementById("test-results");
+		console.log(element.children.length);
+		if (element === null) {
+			alert("Nisem ustvari div wraperja za testiranje");
+			return;
+		}
+		if (element.children.length !== 4) {
+			alert("Nisem ustvaril vseh potrebnih elementov");
+		}
 
+		// TODO: add more:)
+	}
 })();
