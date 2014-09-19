4f153b9a34fbe1cbbfd2dfd886b6829454c3997a
Slakec <slakec@gmail.com>
2014-09-19 23:43:11 +0200

    Moznost loadanja iz localStorage

--- application.js
+++ application.js
@@ -1,37 +1,60 @@
-(function(){
+( function () {
 
-	var Application = function(data){
+	var Application = function (data) {
 
 		this.nastaviDefaults(data);
 
 		// root element kamor ostali appendajo zadeve....
-		this.rootElement = Z.create("div", this.preFix + "-central");;
+		this.rootElement = Z.create("div", this.preFix + "-central", {className: "slidy-central"});;
 
-		setTimeout(function(){
+		setTimeout(function () {
 			Z.fireEvent(Z.body(), new Z.event(Z.body(), "applicationLoaded", data));
 		},0);
+
+		Z.addEvent(Z.body(), "applicationSaveData", this.saveData.bind(this));
+		setTimeout(this.loadData.bind(this, this.projectName), 100);
 	}
 
-	Application.prototype.nastaviDefaults = function(data) {
+	/**
+	 * Preverimo ali imamo v local storage shranjene podatke o sliderjih/expensih
+	 */
+	Application.prototype.loadData = function (projectName) {
+		var data = localStorage.getItem(projectName);
+
+		if (data !== null ){
+			try {
+				var parsedData = JSON.parse(data);
+				var e = new Z.event("applicationDataLoadedFromStorage", parsedData);
+				Z.fireEvent(Z.body(), e);
+			} catch(e) {
+				console.error("Poizkušal sem parsati shranjene podatke v localStorage", data, e);
+			}
+
+		}
+	}
+
+	/**
+	 * Shranimo podatke v storage
+	 */
+	Application.prototype.saveData = function(e) {
+		localStorage.setItem(this.projectName, JSON.stringify(e.data));
+	}
+	Application.prototype.nastaviDefaults = function (data) {
 		for (var def in data) {
 			this[def] = data[def];
 		}
 	}
 
-	window.onload = function(){
+	window.onload = function () {
 		var data = {
 			appMode : "diameter", // TESTING: uporabniki podajo polmer za vsak krog posebaj
 			appRanged: true, // TESTING: uporabnik ne more iti v minus na sliderju
 			izrisovanje: 1, // TESTING: 1 = isti canvas za vse sliderje | 2 = vsak slider na svojem canvasu
 			preFix : "slidy",
-			editModeTimeout: 3000
+			editModeTimeout: 3000,
+			projectName: "slidy"
 		};
 
 		window["application"] = new Application(data);
-
-		setTimeout(function(){
-			var e = new Z.event("expenseAdded", {polmer: 100, range: 720, value: 180, step: 25, premer:200});
-			Z.fireEvent(Z.body(), e);
-		}, 0);
 	}
 })();
